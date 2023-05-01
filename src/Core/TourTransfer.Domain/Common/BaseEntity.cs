b94890aca52d3ffd78921d28a867c0b740f4d280
Furkan Zingal <frkn.zngl1996@hotmail.com>
2023-05-01 22:56:42 +0300

    TourTransfer Api oluşturuldu

---
+++ src/Core/TourTransfer.Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourTransfer.Domain.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime RecordDate { get; set; }
+        public string RecordUser { get; set; } = string.Empty;
+        public DateTime UpdateDate { get; set; }
+        public string UpdateUser { get; set; } = string.Empty;
+        public DateTime DeletedDate { get; set; }
+        public string DeletedUser { get; set; } = string.Empty;
+    }
+}
---
+++ src/Core/TourTransfer.Domain/Entities/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourTransfer.Domain.Common;
+
+namespace TourTransfer.Domain.Entities
+{
+    public class Reservation : BaseEntity
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public Guid Arrival { get; set; }
+        public Guid Departure { get; set; }
+        public Guid CarsId { get; set; }
+        public Guid CustomerId { get; set; }
+        public string Description { get; set; } = "";
+        public int InvoiceTypeId { get; set; }
+        public int Adult { get; set; }
+    }
+}
