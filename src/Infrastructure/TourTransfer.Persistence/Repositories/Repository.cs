f64cc0ed0449e8731493d1146d2f24860285a946
Furkan Zingal <frkn.zngl1996@hotmail.com>
2023-05-03 00:01:39 +0300

    .gitignore

--- src/Infrastructure/TourTransfer.Persistence/Repositories/Repository.cs
+++ src/Infrastructure/TourTransfer.Persistence/Repositories/Repository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TourTransfer.Application.Interfaces.Repository;
 
 namespace TourTransfer.Persistence.Repositories
 {
-    public class Repository
-    {
-    }
+    //public class Repository<T> : IRepository<T> where T : IBase
+    //{
+    //}
 }
