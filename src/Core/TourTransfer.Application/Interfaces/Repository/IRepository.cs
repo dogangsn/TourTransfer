55eacd7dc24faaf7b925dc409fbb9c69fba906f5
Furkan Zingal <frkn.zngl1996@hotmail.com>
2023-05-03 22:38:36 +0300

    repository ve repository interface oluşturuldu

--- src/Core/TourTransfer.Application/Interfaces/Repository/IRepository.cs
+++ src/Core/TourTransfer.Application/Interfaces/Repository/IRepository.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TourTransfer.Application.Interfaces.UnitOfWork;
 using TourTransfer.Domain.Common;
 
 namespace TourTransfer.Application.Interfaces.Repository
 {
-    public interface IRepository<T> where T : BaseEntity, new()
+    public interface IRepository<T> where T : IBase
     {
-        Task<List<T>> GetAsync();
+        IUnitOfWork UnitOfWork { get; }
+
+        Task<List<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        string includeString = null,
+                                        bool disableTracking = true);
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                       Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                       List<Expression<Func<T, object>>> includes = null,
+                                       bool disableTracking = true);
         Task<T> GetByIdAsync(Guid id);
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
         Task<T> AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
--- src/Infrastructure/TourTransfer.Persistence/Repositories/Repository.cs
+++ src/Infrastructure/TourTransfer.Persistence/Repositories/Repository.cs
@@ -1,14 +1,88 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using TourTransfer.Application.Interfaces.Repository;
+using TourTransfer.Application.Interfaces.UnitOfWork;
+using TourTransfer.Domain.Common;
+using TourTransfer.Persistence.Context;
 
 namespace TourTransfer.Persistence.Repositories
 {
-    //public class Repository<T> : IRepository<T> where T : IBase
-    //{
-    //}
-    //test commit
+    public class Repository<T> : IRepository<T> where T : EntityIdBase
+    {
+        protected ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IUnitOfWork UnitOfWork => throw new NotImplementedException();
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().FirstOrDefaultAsync(predicate) ?? throw new ArgumentNullException();
+        }
+
+        public async Task<List<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(includeString)) query = query.Include(includeString);
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (includes != null) query = includes.Aggregate(query, (current, include) => current.Include(include));
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id) ?? throw new ArgumentNullException();
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+            _dbContext.Update(entity).Property(x => x.Id).IsModified = false;
+        }
+    }
 }
